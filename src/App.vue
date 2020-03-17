92f35ba4dcd127229c4a4d617c75968db282b430
yangmengwei <1635433207@qq.com>
2020-03-17 20:27:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <div class="box">
+      我是父组件
+      <p>
+        {{newStr}}
+      </p>
+    </div>
+    <NewVue @zxcv="acceptMsg" />
+  </div>
+</template>
+
+<script>
+import NewVue from './components/newVue'
+export default {
+  name: 'app',
+  components: {
+    NewVue
+  },
+  data() {
+    return {
+      num: 999999,
+      count: 0,
+      test: 10,
+      newStr: ''
+    }
+  },
+  methods: {
+    addCount() {
+      this.count++
+    },
+    acceptMsg(data) {
+      this.newStr = data
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.box{
+  width: 100%;
+  height: 200px;
+  background: skyblue;
+}
+</style>
---
+++ src/components/newVue.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="new">
+        我是子组件
+        <button @click="sendMsg">我是按钮</button>
+        {{str}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+            str: 10
+        }
+    },
+    methods: {
+       sendMsg() {
+           this.str++
+           this.$emit('zxcv', this.str)  
+       }
+    }
+    // props: ['num', 'sfs']
+
+    // props: {
+    //     num: String
+    // }
+
+    // props: {
+    //     num: {
+    //         // 类型
+    //         type: Number,
+    //         // 是否必传
+    //         required: false,
+    //         // 默认值
+    //         default: 1000000000000000
+    //     }
+    // }
+}
+</script>
+
+<style scoped>
+    .new{
+        width: 100%;
+        height: 200px;
+        background: orange;
+    }
+</style>
